97fa60de76a52979eab0db043a0b8f1c0077e810
a.mikhailov <santus20111@gmail.com>
2019-01-31 13:05:11 +0300

    map

--- src/main/java/test/skb/test/models/Users.java
+++ src/main/java/test/skb/test/models/Users.java
@@ -2,10 +2,10 @@ package test.skb.test.models;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
-import java.util.Optional;
+import java.util.Map;
 
 public class Users {
-    private static HashMap<String, User> userHashMap = new HashMap<>();
+    private static Map<String, User> userHashMap = new HashMap<>();
     static {
         addUser(new User("1", "Александр Михайлов", LocalDateTime.of(1998, 3, 28, 0, 0, 0)));
         addUser(new User("2", "Иван Михайлов", LocalDateTime.of(1997, 3, 28, 0, 0, 0)));
